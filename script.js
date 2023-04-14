f9d297695e95a44c04505ece25071c4460b7788a
komala1219 <123307320+komala1219@users.noreply.github.com>
2023-04-14 16:22:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+window.addEventListener('resize', function() {
+  var width = window.innerWidth;
+  var height = window.innerHeight;
+  var sizeInfo = document.getElementById('sizeInfo');
+  sizeInfo.innerHTML = 'Width: ' + width + 'px, Height: ' + height + 'px';
+});
+
