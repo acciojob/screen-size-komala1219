b12c8858cfdf77c8629c803cda22500f790774ab
komala1219 <123307320+komala1219@users.noreply.github.com>
2023-04-15 21:34:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 //your JS code here. If required.
-window.addEventListener('resize', function() {
-  var width = window.innerWidth;
-  var height = window.innerHeight;
-  var sizeInfo = document.getElementById('sizeInfo');
-  sizeInfo.innerHTML = 'Width: ' + width + 'px, Height: ' + height + 'px';
-});
+// window.addEventListener('resize', function() {
+//   var width = window.innerWidth;
+//   var height = window.innerHeight;
+//   var sizeInfo = document.getElementById('sizeInfo');
+//   sizeInfo.innerHTML = 'Width: ' + width + 'px, Height: ' + height + 'px';
+// });
+ const sizeInfo = document.querySelector("#sizeInfo h1");
 
+      function updateSize() {
+        const width = window.innerWidth;
+        const height = window.innerHeight;
+        sizeInfo.textContent = `Width: ${width}px, Height: ${height}px`;
+      }
+
+      window.addEventListener("resize", updateSize);
+      updateSize();
